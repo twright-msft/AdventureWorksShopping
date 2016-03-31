a4b81fd64ea75b3ec93832fc402b2ae37be171b9
Travis Wright <twright@microsoft.com>
2016-03-31 04:33:46 -0700

    Initial Commit

---
+++ AdventureWorksShopping/Controllers/MessagesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Utilities;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AdventureWorksShopping
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                // get the products list and return it to the user
+                string products = GetProducts(message.Text);
+                return message.CreateReplyMessage(products);
+                //return message.CreateReplyMessage("ack");
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+            }
+        }
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+
+        private static string GetProducts(string searchTerm)
+        {
+            string queryString = "SELECT Name FROM Production.Product WHERE Name LIKE @searchTerm";
+            string connectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+            string products = "";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@searchTerm", "%" + searchTerm + "%");
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        products += reader.GetString(0);
+                        products += ",";
+                    }
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+                finally
+                {
+                    reader.Close();
+                }
+
+                return products.TrimEnd(',');
+            }
+        }
+    }
+}
